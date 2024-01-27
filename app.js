08d0f927daabe4d5a9f84c668ccc32e2c763234a
Nick Kim <khs10@hotmail.com>
2024-01-27 23:33:10 +0800

    changes made with launchdarkly demo

--- app.js
+++ app.js
@@ -5,6 +5,31 @@ const router = express.Router();
 const path = __dirname + '/views/';
 const port = 8080;
 
+//LaunchDarkly addition
+const LaunchDarkly = require('@launchdarkly/node-server-sdk');
+
+// Set featureFlagKey to the feature flag key you want to evaluate.
+const featureFlagKey = 'dark-theme';
+// Set sdkKey to your LaunchDarkly SDK key.
+// const sdkKey = 'sdk-3f2ce257-ecb3-4373-9a48-ac000d67ed86';
+
+const context = {
+  "kind": 'user',
+  "membership" : 'vip',
+  "key": 'user-key-123abc',
+  "name": 'nick'
+};
+
+const client = LaunchDarkly.init('sdk-3f2ce257-ecb3-4373-9a48-ac000d67ed86');
+
+
+function showMessage(s) {
+  console.log("*** " + s);
+  console.log("");
+}
+
+
+
 router.use(function (req,res,next) {
   console.log('/' + req.method + ' called') ;
   next();
@@ -15,7 +40,20 @@ router.get('/', function(req,res){
 });
 
 router.get('/sharks', function(req,res){
-  res.sendFile(path + 'sharks.html');
+  client.waitForInitialization().then(function () {
+    showMessage("SDK successfully initialized!");
+    client.variation(featureFlagKey, context, false, function (err, flagValue) {
+      showMessage("Feature flag '" + featureFlagKey + "' is " + flagValue + " for this context");
+    if(featureFlagKey)
+    {    
+      res.sendFile(path + 'sharks.html');
+    }
+    else 
+    {
+      res.sendFile(path + 'sharks2.html');
+    }
+    })
+  })
 });
 
 app.use(express.static(path));
